8c7410180f045637796def43eae68a92f296772f
Zuhair Hasan Raza <zuhairhasan2003@gmail.com>
2026-07-25 03:25:20 +0100

    Actual lights functionality added using GPIO

--- Pi/lights.c
+++ Pi/lights.c
@@ -3,7 +3,8 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <sys/un.h>
-#include<cjson/cJSON.h>
+#include <cjson/cJSON.h>
+#include <gpiod.h>
 
 int main()
 {
@@ -18,6 +19,12 @@ int main()
 
     printf("Connected to the server!!!\n");
 
+    const char * chip_name = "gpiochip0";
+    struct gpiod_chip * chip = gpiod_chip_open_by_name(chip_name);
+    unsigned int pin_offset = 5;
+    struct gpiod_line * line = gpiod_chip_get_line(chip, pin_offset);
+    gpiod_line_request_output(line, "led", 0);
+
     while(1)
     {
         char data[1024] = { 0 };
@@ -30,6 +37,15 @@ int main()
         printf("%s\n", cJSON_GetObjectItemCaseSensitive(parsed_json, "service")->valuestring);
         printf("%s\n", cJSON_GetObjectItemCaseSensitive(parsed_json, "operation")->valuestring);
         printf("%s\n", cJSON_GetObjectItemCaseSensitive(parsed_json, "user_input")->valuestring);
+
+        if(cJSON_GetObjectItemCaseSensitive(parsed_json, "operation")->valuestring == "turn_on")
+        {
+            gpiod_line_set_value(line, 1);
+        }
+        else if(cJSON_GetObjectItemCaseSensitive(parsed_json, "operation")->valuestring == "turn_off")
+        {
+            gpiod_line_set_value(line, 0);
+        }
     }
 
     return 0;
